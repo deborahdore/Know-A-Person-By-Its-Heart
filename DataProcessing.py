a02d9dbf2976a79fb12c72e767524ded7ddd0141
deborahdore <deborah.dore@outlook.com>
2021-12-21 17:52:02 +0100

    ancora il dataset non ha finito fra l'altro

--- DataProcessing.py
+++ DataProcessing.py
@@ -22,9 +22,9 @@ def main():
     denoised_ecg = lfilter(LowPassFilter(), 1, denoised_ecg)
 
     cleaned_signal = SmoothSignal(denoised_ecg)
-    # cleaned_signal = cleaned_signal[2400:]
 
     plt.clf()
+
     # plt.plot(signal, label="RAW ECG")
     # plt.plot(cleaned_signal, label="Cleaned ECG", color='k')
     # plt.grid(True)
@@ -33,44 +33,39 @@ def main():
 
     # only keep best r peaks with prominence = 1
     r_peak, _ = find_peaks(cleaned_signal, prominence=1, distance=150)
-
-    # segment the signal
-    signal_dwt, waves_cwt = nk.ecg_delineate(cleaned_signal, rpeaks=r_peak, sampling_rate=1000, method="cwt",
-                                             show=False,
+    signal_dwt, waves_dwt = nk.ecg_delineate(cleaned_signal, rpeaks=r_peak, sampling_rate=1000, method="dwt", show=True,
                                              show_type='peaks')
 
-    t_peaks = [n for n in waves_cwt['ECG_T_Peaks'] if not math.isnan(n)]
-    p_peaks = [n for n in waves_cwt['ECG_P_Peaks'] if not math.isnan(n)]
-    q_peak = [n for n in waves_cwt['ECG_Q_Peaks'] if not math.isnan(n)]
-    s_peak = [n for n in waves_cwt['ECG_S_Peaks'] if not math.isnan(n)]
-
-    #
-    # plot = nk.events_plot([waves_cwt['ECG_T_Peaks'],
-    #                        waves_cwt['ECG_P_Peaks'],
-    #                        waves_cwt['ECG_Q_Peaks'],
-    #                        waves_cwt['ECG_S_Peaks']], cleaned_signal[:12500])
-    # plt.xlim(11000, 12500)
-    # plt.ylim(-0.1, 0.2)
+    t_peaks = [n for n in waves_dwt['ECG_T_Peaks'] if not math.isnan(n)]
+    p_peaks = [n for n in waves_dwt['ECG_P_Peaks'] if not math.isnan(n)]
+    q_peaks = [n for n in waves_dwt['ECG_Q_Peaks'] if not math.isnan(n)]
+    s_peaks = [n for n in waves_dwt['ECG_S_Peaks'] if not math.isnan(n)]
 
     Tx = mean(peak_widths(cleaned_signal, t_peaks))
     Px = mean(peak_widths(cleaned_signal, p_peaks))
-    Qx = mean(peak_widths(cleaned_signal, q_peak))
-    Sx = mean(peak_widths(cleaned_signal, s_peak))
+    Qx = mean(peak_widths(cleaned_signal, q_peaks))
+    Sx = mean(peak_widths(cleaned_signal, s_peaks))
 
     Ty = mean(peak_prominences(cleaned_signal, t_peaks))
     Py = mean(peak_prominences(cleaned_signal, p_peaks))
-    Qy = mean(peak_prominences(cleaned_signal, q_peak))
-    Sy = mean(peak_prominences(cleaned_signal, s_peak))
+    Qy = mean(peak_prominences(cleaned_signal, q_peaks))
+    Sy = mean(peak_prominences(cleaned_signal, s_peaks))
 
-    # plt.plot(cleaned_signal)
-    # plt.plot(t_peaks, cleaned_signal[t_peaks], "x", label="t_peaks")
-    # plt.plot(p_peaks, cleaned_signal[p_peaks], "x", label="p_peaks")
-    # plt.plot(q_peak, cleaned_signal[q_peak], "x", label="q_peak")
-    # plt.plot(s_peak, cleaned_signal[s_peak], "x", label="s_peak")
-    #
-    # plt.legend()
-    # plt.grid(True)
-    # plt.xlim(0,5000)
-    # plt.ylim(-1,1.5)
+    plt.clf()
+    plt.plot(cleaned_signal)
+    plt.plot(t_peaks, cleaned_signal[t_peaks], "x", label="t_peaks")
+    plt.plot(p_peaks, cleaned_signal[p_peaks], "x", label="p_peaks")
+    plt.plot(q_peaks, cleaned_signal[q_peaks], "x", label="q_peaks")
+    plt.plot(s_peaks, cleaned_signal[s_peaks], "x", label="s_peak")
+    plt.legend()
+    plt.xlim(0, 5000)
+    plt.grid(True)
+
+    # contiene tutti i picchi in ordine P,Q,S,T in ripetizione
+    final_peaks = p_peaks
+    final_peaks.extend(t_peaks)
+    final_peaks.extend(q_peaks)
+    final_peaks.extend(s_peaks)
+    final_peaks.sort()
 
     plt.show()
