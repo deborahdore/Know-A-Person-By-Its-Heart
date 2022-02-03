a9d29360f09fc9f96bab09ff175df788223e278d
Deborah Dore <dore.1994616@studenti.uniroma1.it>
2022-02-03 17:15:54 +0100

    ROC, FAR, FRR

--- Evaluation_biometric.py
+++ Evaluation_biometric.py
@@ -6,7 +6,7 @@ from sklearn.metrics import auc, roc_curve
 from sklearn.model_selection import train_test_split
 
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, label_binarize
+from sklearn.preprocessing import MinMaxScaler, label_binarize
 
 
 def eval(dataset="datasets/balanced_dataset.csv"):
@@ -32,25 +32,72 @@ def eval(dataset="datasets/balanced_dataset.csv"):
 
 
 def ROC_curve(n_classes, y_score, y_test):
-    # Compute ROC curve and ROC area for each class
-    fpr = dict()
+    far = dict()
     tpr = dict()
     roc_auc = dict()
-    print(y_test)
+    thresholds = dict()
+    frr = dict()
+    eer = dict()
+
     for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
-        roc_auc[i] = auc(fpr[i], tpr[i])
+        far[i], tpr[i], thresholds[i] = roc_curve(y_test[:, i], y_score[:, i])
+        frr[i] = 1 - tpr[i]
+        # eer[i] = far[i][np.nanargmin(np.absolute((frr[i] - far[i])))]
+        roc_auc[i] = auc(far[i], tpr[i])
     # Plot of a ROC curve for a specific class
+    specific_class = [0, 5, 22]
+    for i in specific_class:
+        plt.figure()
+        plt.plot(far[i], tpr[i], label='ROC curve (area = %0.2f)' % roc_auc[i])
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+        plt.tight_layout()
+        plt.savefig('plot/ROC/ROC_curve' + str(i) + '.svg', dpi=1200)
+        plt.clf()
+    # plot overall curve
+    plt.figure(figsize=(10, 10))
     for i in range(n_classes):
-        plt.plot(fpr[i], tpr[i], label='ROC curve (area = %0.2f)' % roc_auc[i])
+        plt.plot(far[i], tpr[i], label='ROC curve (area = %0.2f)' % roc_auc[i])
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
-    plt.legend(loc="lower right")
-    plt.show()
+    plt.title('Receiver operating characteristic')
+    plt.tight_layout()
+    plt.savefig('plot/ROC/ROC_curve.svg', dpi=1200)
+    plt.clf()
+
+    # plot FAR
+    for i in range(n_classes):
+        plt.plot(thresholds[i], far[i])
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Threshold')
+    plt.ylabel('Equal Error Rate')
+    plt.title('False Acceptance Rate')
+    plt.tight_layout()
+    plt.savefig('plot/FAR_FRR/FAR_curve.svg', dpi=1200)
+    plt.clf()
+
+    # plot FRR against threshold
+    for i in range(n_classes):
+        plt.plot(thresholds[i], frr[i])
+
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Threshold')
+    plt.ylabel('False Rejection Rate')
+    plt.title('FRR')
+    plt.tight_layout()
+    plt.savefig('plot/FAR_FRR/FFR_curve.svg', dpi=1200)
+    plt.clf()
+
 
 
 if __name__ == '__main__':
